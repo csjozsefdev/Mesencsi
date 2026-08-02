f945149fa4854febac08b21117342986139217df
József Csordás <csjozsefdev@gmail.com>
2026-08-02 14:59:54 +0200

    Add approved 2026-07-13 ASZF

--- project/backend/policy_versions.py
+++ project/backend/policy_versions.py
@@ -1,5 +1,5 @@
 """Server-owned legal policy versions persisted with user acknowledgements."""
 
 COOKIE_POLICY_VERSION = "2026-06-14"
-TERMS_VERSION = "2026-06-14"
+TERMS_VERSION = "2026-07-13"
 PRIVACY_POLICY_VERSION = "2026-06-14"
