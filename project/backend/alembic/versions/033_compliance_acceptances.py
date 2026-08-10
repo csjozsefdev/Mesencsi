5905f2410281605a93d8bff668f8c206cba642b5
root <root@h104-136.rackhostvps.com>
2026-08-10 13:12:42 +0000

    Fix compliance migration revision chain

--- project/backend/alembic/versions/033_compliance_acceptances.py
+++ project/backend/alembic/versions/033_compliance_acceptances.py
@@ -10,8 +10,8 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import inspect
 
-revision: str = "025"
-down_revision: Union[str, None] = "024"
+revision: str = "033"
+down_revision: Union[str, None] = "032"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
